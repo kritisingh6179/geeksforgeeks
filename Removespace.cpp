2a148c69c793ba48ae561cd2d080c68b96f008bc
kriti singh <77061475+kritisingh6179@users.noreply.github.com>
2023-05-23 22:38:31 +0530

    String

---
+++ Removespace.cpp
@@ -0,0 +1,13 @@
+// Given a string, remove spaces from it. 
+
+// Example 1:
+
+// Input:
+// S = "geeks  for geeks"
+// Output: geeksforgeeks
+// Explanation: All the spaces have been 
+// removed.
+
+// Method 1:-
+
+  
